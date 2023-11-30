92952cb59e17ee2034268d1ac56be2e5e084e077
Julia Zdzilowska <135221630+jzdzilowska@users.noreply.github.com>
2023-11-30 01:36:41 -0500

    feat: form updates, dashboard nearly done, po sting not functional

--- front/server.js
+++ front/server.js
@@ -3,6 +3,7 @@ import { join } from "path"; // Import the path module
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import cors from "cors";
+import fs from "fs";
 
 //create local server to host files in project directory
 const __filename = fileURLToPath(import.meta.url);
@@ -16,6 +17,27 @@ const port = 2000;
 app.get("/login", (req, res) => {
   const filePath = join(__dirname, "/public/mockLogin.json");
   res.sendFile(filePath);
+});
+
+app.get("/getPartner", (req, res) => {
+  const filePath = join(__dirname, "/public/mockPartner.json");
+  res.sendFile(filePath);
+});
+
+app.post("/submitForm", express.json(), (req, res) => {
+  const { data } = req.body;
+  if (!data) {
+    return res.status(400).json({ error: "Data is required." });
+  }
+  // Convert data to CSV format (assuming data is an array of objects)
+  const csvContent = data
+    .map((item) => Object.values(item).join(","))
+    .join("\n");
+
+  const csvFilePath = join(__dirname, "/public/formData.csv");
+  fs.writeFileSync(csvFilePath, csvContent);
+
+  res.json({ success: true });
 });
 
 // Start the server
--- front/src/components/Dashboard.tsx
+++ front/src/components/Dashboard.tsx
@@ -4,17 +4,83 @@ import { UserRole, userRoleState, userState } from "../recoil/atoms";
 import "../styles/Dashboard.css";
 import { useNavigate } from "react-router-dom";
 import Timer from "./Timer";
+import "../styles/nightsky.scss";
 
 const Dashboard = () => {
   const user = useRecoilValue(userState);
   const userRole = useRecoilValue(userRoleState);
   const navigate = useNavigate();
+  const [partner, setPartner] = useState<string | null>(null);
+  const [issue, setIssue] = useState<string | null>(null);
+  const [bugCategory, setBugCategory] = useState("");
+  const [debuggingProcess, setDebuggingProcess] = useState("");
 
   useEffect(() => {
     if (user === null) {
       navigate("/login");
     }
   }, [userState, userRoleState, user]);
+
+  useEffect(() => {
+    const fetchPartner = async () => {
+      try {
+        const response = await fetch("http://localhost:2000/getPartner");
+        const data = await response.json();
+        const partner = data.partner;
+        const issue = data.issue;
+        setPartner(partner);
+        setIssue(issue);
+      } catch (error) {
+        console.error("Error fetching partner:", error);
+      }
+    };
+
+    if (userRole.role === UserRole.DebuggingPartner) {
+      fetchPartner();
+    }
+  }, [userRole.role]);
+
+  const openResourcesWebsite = () => {
+    const url: string = "https://hackmd.io/@brown-csci0320/BJKCtyxxs/";
+    window.open(url, "_blank");
+  };
+
+  const handleSubmit = async () => {
+    if (user && partner) {
+      console.log(user.email, partner, bugCategory, debuggingProcess);
+      try {
+        const response = await fetch("http://localhost:2000/submitForm", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            user: user.email,
+            partner: partner,
+            bugCategory: bugCategory,
+            debuggingProcess: debuggingProcess,
+          }),
+        });
+
+        if (!response.ok) {
+          // If the server returns an error status
+          throw new Error(`Server error: ${response.status}`);
+        } else {
+          const result = await response.json();
+          if (result.success) {
+            setPartner(null);
+            setBugCategory("");
+            setDebuggingProcess("");
+          } else {
+            // Handle other success scenarios or display an error message
+            alert("Form submission failed:" + result);
+          }
+        }
+      } catch (error) {
+        alert("Error encountered: " + error);
+      }
+    }
+  };
 
   const renderHeaderBasedOnRole = (role: UserRole) => {
     switch (role) {
@@ -41,7 +107,48 @@ const Dashboard = () => {
       case UserRole.Instructor:
         return <p>instructor content here.</p>;
       case UserRole.DebuggingPartner:
-        return <p>debugging partner content here.</p>;
+        return (
+          <div className="debugging-partner-content">
+            <div className="other-partner-info">
+              <p>
+                Your help requester is:{" "}
+                <b> {partner ? partner : "No one yet!"}</b>
+              </p>
+              {partner && (
+                <p>
+                  Issue category: <b>{issue}</b>
+                </p>
+              )}
+            </div>
+            <div className="debugging-form">
+              <div className="bug-category">
+                <p>
+                  <b>Bug category: </b>
+                </p>
+                <input
+                  type="bugCategory"
+                  placeholder="NullPointerException"
+                  value={bugCategory}
+                  onChange={(e) => setBugCategory(e.target.value)}
+                />
+              </div>
+              <div className="debugging-process">
+                <p>
+                  <b>Debugging process: </b>
+                </p>
+                <input
+                  type="debuggingProcess"
+                  placeholder="Strategically placed print statements to trace the bug source, then..."
+                  value={debuggingProcess}
+                  onChange={(e) => setDebuggingProcess(e.target.value)}
+                />
+              </div>
+              <button className="submit" onClick={handleSubmit}>
+                Submit!
+              </button>
+            </div>
+          </div>
+        );
       case UserRole.HelpRequester:
         return <p>help requester content here.</p>;
       default:
@@ -50,13 +157,39 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="dashboard-body">
-      {renderHeaderBasedOnRole(userRole.role)}
-      <div className="dashboard-container">
-        <div>
-          <h1>Welcome, {user?.name.split(" ")[0]}!</h1>
+    <div className="body">
+      <div id="stars-container">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+        <div className="dashboard-body">
+          {renderHeaderBasedOnRole(userRole.role)}
+          <div className="dashboard-container">
+            <div className="welcome-container">
+              <h1>Welcome, {user?.name.split(" ")[0]}!</h1>
+              <button className="resources">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="resources-icon"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="#000000"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  onClick={openResourcesWebsite}
+                >
+                  <circle cx="12" cy="12" r="10"></circle>
+                  <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
+                  <line x1="12" y1="17" x2="12.01" y2="17"></line>
+                </svg>
+              </button>
+            </div>
+            {renderContentBasedOnRole(userRole.role)}
+          </div>
         </div>
-        {renderContentBasedOnRole(userRole.role)}
       </div>
     </div>
   );
