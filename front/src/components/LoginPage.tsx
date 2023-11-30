d7e4cfa01ef558c1e8e1e73dbc7ba9ae89f4debf
rdbrooks <rachel_brooks@brown.edu>
2023-11-29 23:22:31 -0500

    feat: can switch between mocked login/firebase login and firebase login navigates to appropriate pages

--- front/src/components/LoginPage.tsx
+++ front/src/components/LoginPage.tsx
@@ -2,9 +2,27 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/LoginPage.css";
 import { useRecoilState, useSetRecoilState } from "recoil";
-import { userRoleState, userState } from "../recoil/atoms";
+import { mockedMode, userRoleState, userState } from "../recoil/atoms";
 import { UserRole } from "../recoil/atoms";
 import { IUser } from "../types/IUser";
+
+// Used the following video for firebase authentication: https://www.youtube.com/watch?v=vDT7EnUpEoo
+// Added to import from other services
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+// Import functions needed from appropriate SDKs
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCpEp6SMcO3hen7UKMeR-NNEy7ySSWStYI",
+  authDomain: "collab-section-manager.firebaseapp.com",
+  projectId: "collab-section-manager",
+  storageBucket: "collab-section-manager.appspot.com",
+  messagingSenderId: "682750943740",
+  appId: "1:682750943740:web:36bb8a15ff7bea374fd279",
+  measurementId: "G-1FFWDE2HTT",
+};
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -13,13 +31,18 @@ const LoginPage = () => {
   const setUser = useSetRecoilState(userState);
   const [userRole, setUserRole] = useRecoilState(userRoleState);
 
-  const handleLogin = async () => {
+  const handleLoginMocked = async () => {
     const response = await fetch("http://localhost:2000/login/");
     const data = await response.json();
     const users: IUser[] = data.users;
 
     const user = users.find((u) => u.email === email);
     console.log(email);
+
+    // if not brown.edu email then navigate to failure page
+    if (!user?.email.includes("brown.edu")) {
+      return navigate("/failed-login");
+    }
 
     if (user) {
       setUser(user);
@@ -36,32 +59,85 @@ const LoginPage = () => {
     }
   };
 
-  return (
-    <div className="login-body">
-      <div className="login-container">
-        <h1>Welcome to Debugging Helper</h1>
-        <div className="input-box">
-          <input
-            type="email"
-            placeholder="username"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+  // Initialize Firebase login and redirect to appropriate page
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+
+      const name: string | null = result.user.displayName;
+      const email: string | null = result.user.email;
+      console.log(name);
+      console.log(email);
+
+      if (name == null || email == null) {
+        console.log("error: name of email is null");
+      } else {
+        const user: IUser = {
+          email: email,
+          name: name,
+          role: "instructor", // will need to determine from csv file
+        };
+
+        setUser(user);
+        console.log(user);
+        console.log(user.role);
+
+        if (user.role === "student") {
+          // pass user information to the further component
+          return navigate("/role-selection", { state: { user } });
+        } else if (user.role === "instructor") {
+          setUserRole({ role: UserRole.Instructor, time: null });
+          return navigate("/dashboard");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (!mockedMode) {
+    return (
+      <div className="login-body">
+        <div className="login-container">
+          <h1>Welcome to Debugging Helper</h1>
+          <button onClick={signInWithGoogle} className="btn">
+            Sign In With Google
+          </button>
         </div>
-        <div className="input-box">
-          <input
-            type="password"
-            placeholder="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button onClick={handleLogin} className="btn">
-          Login
-        </button>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="login-body">
+        <div className="login-container">
+          <h1>Welcome to Debugging Helper</h1>
+          <div className="input-box">
+            <input
+              type="email"
+              placeholder="username"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-box">
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button onClick={handleLoginMocked} className="btn">
+            Login
+          </button>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default LoginPage;
